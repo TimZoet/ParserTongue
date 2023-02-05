c90e9f066c242a1ff48e42d8cde51fb23d733105
TimZoet <ts.zoet@gmail.com>
2023-02-05 15:47:25 +0100

    Fixed export_sources.

--- conanfile.py
+++ conanfile.py
@@ -42,10 +42,9 @@ class ParserTongueConan(ConanFile):
     def export_sources(self):
         copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
         copy(self, "license", self.recipe_folder, self.export_sources_folder)
+        copy(self, "parsertongueVersionString.cmake", self.recipe_folder, self.export_sources_folder)
         copy(self, "readme.md", self.recipe_folder, self.export_sources_folder)
-        copy(self, "cmake/*", self.recipe_folder, self.export_sources_folder)
-        copy(self, "modules/CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
-        copy(self, "modules/parsertongue/*", self.recipe_folder, self.export_sources_folder)
+        copy(self, "modules/*", self.recipe_folder, self.export_sources_folder)
     
     def config_options(self):
         base = self.python_requires["pyreq"].module.BaseConan
